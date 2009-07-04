93a220ee0707bc2f22188d6b179786c50b513383
M Omar Faruque Sarker <writefaruq@gmail.com>
2009-07-04 16:33:53 +0100

    Working device creation, samll  bug in xml parser

---
+++ src/test-exe.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+rm -rf /tmp/dmtx*
+killall dmtxpluginfront
+mkdir /tmp/dmtxdatadir
+mkdir /tmp/dmtxsymboldir
+mkdir /tmp/dmtxoutputdir
+#cp ./sample-symbol.png /tmp/dmtxsymboldir/
+./dmtxpluginfront  ../sample-symbol2.png
+#ls -l /tmp/dmtxoutputdir/output.txt
+#cat /tmp/dmtxdatadir/dmtxd.lock
+#cat /tmp/dmtxdatadir/dmtxd.log
+#cat /tmp/dmtxoutputdir/symbol.txt
+
+
